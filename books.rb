bd6eb72b2cd2eb0665805b1f05a8e58a0f63e82f
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-10-11 00:29:58 -0300

    Add books.rb

---
+++ books.rb
@@ -0,0 +1,31 @@
+require_relative 'book'
+
+class Books
+  attr_accessor :books
+  def initialize
+    @books = []
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+
+    book = Book.new(title, author)
+    @books.push(book)
+
+    puts 'Book created successfully'
+  end
+
+  def list_books
+    if @books.empty?
+      puts 'No book added in the list yet'
+      return
+    end
+    @books.map do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+  end
+end
