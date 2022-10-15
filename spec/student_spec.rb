3deed48ca872c8dc4bbb7e0c90a0338363dfccfa
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-10-15 20:08:46 -0300

    Add student testing

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,17 +2,21 @@ require_relative '../student'
 
 describe Student do
   context 'When creating a Student' do
-    student = Student.new(classroom: 'Chemistry', age: 31, id: nil, name: 'Jean', parent_permission: true)
-    it "Should return 'Elias' as the student name" do
-      expect(student.name).to eq 'Elias'
+    student = Student.new(nil, 31, 'Jean', false)
+    it "Should return 'Jean' as the student name" do
+      expect(student.name).to eq 'Jean'
     end
 
     it "Should return '31' as the Student age" do
       expect(student.age).to eq 31
     end
 
+    it "Should return false as the Student parent_permission" do
+      expect(student.parent_permission).to eq false
+    end
+
     it "Should return '¯\(ツ)/¯' as the student name" do
-      expect(student.play_hooky).to eq '¯\(ツ)/¯'
+      expect(student.play_hook).to eq "¯\(ツ)/¯"
     end
   end
 end
