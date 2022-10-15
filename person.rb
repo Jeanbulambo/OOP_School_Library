2b3c64c3ee9a49b854447f19119624a3ed49b03d
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-10-15 18:37:36 -0300

    Update persons.rb and person.rb

--- person.rb
+++ person.rb
@@ -2,23 +2,14 @@ require_relative './nameable'
 
 class Person < Nameable
   attr_reader :id, :rentals
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
-  end
-  # exporting data
-  def export_json
-    {
-      'name' => @name,
-      'age' => @age,
-      'parent_permission' => @parent_permission
-    }
   end
 
   def can_use_services?
