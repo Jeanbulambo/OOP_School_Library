6b33fa87e69904ec36fcaf58ab28e6f5a84639dd
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-10-15 20:08:55 -0300

    Add teacher testing

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,20 +1,19 @@
 require_relative '../teacher'
 
-describe ' For creating a teacher' do
-  context 'when creating a teacher' do
-    teacher = Teacher.new('Chemistry', 31, 'Jean')
+describe Teacher do
+  context 'When creating a teacher' do
+    teacher = Teacher.new(nil, 31, 'Chemistry', 'Jean')
 
     it "should return 'Jean' as a teacher name" do
-     puts teacher.name
      expect(teacher.name).to eq 'Jean'
     end
-  
+
     it "should return '31' as a teacher age" do
       expect(teacher.age).to eq 31
     end
 
    it "should return 'Chemistry' as a teacher specialization" do
-    expect(teacher.specialty).to eq 'Chemistry'
+    expect(teacher.specialization).to eq 'Chemistry'
    end
 
     it "can_use_services method from teacher class should return true" do
